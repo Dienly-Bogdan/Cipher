5da4215cac85c10c5b93445d5c1614a4d6e5c8a2
Egor <egor.egor.3.timofeev@mail.ru>
2024-01-31 17:55:28 +0300

    cipher_decoding
    расшифровка строки

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Задача Расшифровать и Зашрифовать/ Строки и Возраст. 
+def cipher_decoding(memory,step):
+    plain_text = ""
+    for c in memory:
+        c_unicode = ord(c)
+        c_index = ord(c) - ord("а")
+        new_index = (c_index - step) % 33
+        new_unicode = new_index + ord("а")
+        new_character = chr(new_unicode)
+        plain_text = plain_text + new_character
+    return plain_text
