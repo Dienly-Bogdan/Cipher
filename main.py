e228c29692ac91d2f5efec872748c91e37a20053
Bogdan Dienly <b_kulinich@list.ru>
2024-01-31 18:08:24 +0300

    Исправлены названия Функции

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # Функция Расшифровки Числа
 def decipher_number(memory, step):
     return memory - step
-# Задача Расшифровать и Зашрифовать/ Строки и Возраст.
 
 def cipher_line(memory,step):
     new_word = ""
@@ -13,8 +12,7 @@ def cipher_line(memory,step):
             new_word += chr(code)
     return new_word
 
-
-def cipher_decoding(memory,step):
+def decipher_line(memory,step):
     plain_text = ""
     for c in memory:
         c_unicode = ord(c)
