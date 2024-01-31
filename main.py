0e4e429973f71eb2146e25edb895d6b1ef479a5f
Muhammad <m_cherchiev_08@mai.ru>
2024-01-31 17:54:28 +0300

    Добавлена функция шифрования строк
    она шифрует прописные русские буквы на определённый шаг

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Задача Расшифровать и Зашрифовать/ Строки и Возраст. 
+# Задача Расшифровать и Зашрифовать/ Строки и Возраст.
+
+def cipher_line(memory,step):
+    new_word = ""
+    for letter in memory:
+        lst = list(range(1072,1103))
+        if ord(letter) in lst:
+            code = ord(letter) + step
+            new_word += chr(code)
+    return new_word
+
+
